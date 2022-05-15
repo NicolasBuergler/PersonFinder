608852623442f80a080cf4b4d89dcdd2d6be17b2
NicolasBuergler <54268181+NicolasBuergler@users.noreply.github.com>
2022-05-15 17:06:56 +0200

    Initialize

---
+++ ionic app/person-finder-pwa/src/app/components/compass/compass.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compass',
+  templateUrl: './compass.component.html',
+  styleUrls: ['./compass.component.scss'],
+})
+export class CompassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
